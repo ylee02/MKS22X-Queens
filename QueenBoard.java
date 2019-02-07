ed7477a39356fc4695abaf306105884b6d45cb57
Yongho Lee <ylee01@stuy.edu>
2019-02-07 09:37:18 -0500

    Added function heads

---
+++ QueenBoard.java
@@ -0,0 +1,26 @@
+public class QueenBoard {
+  private int[][]board;
+
+  public QueenBoard(int size) {
+
+  }
+  private boolean addQueen(int r, int c) {
+
+  }
+
+  private boolean removeQueen(int r, int c) {
+
+  }
+  public String toString() {
+
+  }
+
+  public boolean solve() {
+
+  }
+
+  public int countSolutions() {
+
+  }
+  
+}
