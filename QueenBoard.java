0fd41e7bfce26526c802177d6641f58c615033b9
Yongho Lee <ylee01@stuy.edu>
2019-02-08 10:46:00 -0500

    Added addQueen and removeQueen

--- QueenBoard.java
+++ QueenBoard.java
@@ -1,26 +1,59 @@
+import java.util.Arrays;
 public class QueenBoard {
   private int[][]board;
 
+
+
   public QueenBoard(int size) {
-
+    board = new int[size][size];
   }
-  private boolean addQueen(int r, int c) {
-
+  public boolean addQueen(int r, int c) {
+    if (board[r][c] == 0) {
+      board[r][c] = -1;
+      for (int i = 0; i < board.length; i++) {
+        for (int y = 0; y < board[r].length; y++) {
+          if ((i == r || (i - r == y - c)) && (y > c || i > r))  {
+            board[i][y] += 1;
+          }
+        }
+      }
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
   private boolean removeQueen(int r, int c) {
+    if (board[r][c] == -1) {
+      board[r][c] = 0;
+      for (int i = 0; i < board.length; i++) {
+        for (int y = 0; y < board[r].length; y++) {
+          if (i == r || (i - r == y - c)) {
+            board[i][y] -= 1;
+          }
+        }
+      }
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 
+  public int[][] getBoard() {
+    return board;
   }
   public String toString() {
-
+    return "";
   }
 
   public boolean solve() {
-
+    return true;
   }
 
   public int countSolutions() {
-
+    return 0;
   }
-  
+
 }
